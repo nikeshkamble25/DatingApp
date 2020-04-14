021cacb4d5fa0a726b94bfe4cfbcb9d2a8f25fb9
nikeshkamble25 <nikeshkamble25@gmail.com>
2020-04-14 12:28:54 +0530

    Updated configuation file

--- DatingApp-SPA/src/environments/environment.ts
+++ DatingApp-SPA/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: "https://datingappv8.azurewebsites.net/api/"
+  production: true,
+  apiUrl: "api/"
 };
 
 /*
